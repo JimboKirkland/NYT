825ca2752eddb6ce478bfdf63113c143d6735a91
JimboKirkland <31573144+JimboKirkland@users.noreply.github.com>
2018-01-04 21:31:49 -0500

    Add files via upload

--- NYT/NYT.js
+++ NYT/NYT.js
@@ -10,6 +10,29 @@ var startYear = $("input").eq(2).val();
 
 var endYear = $("input").eq(3).val();
 
+/*var searchTerm = "white house";
+var startYear ='';
+var records = '';
+var endYear ='';
+*/
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+url += '?' + $.param({
+ 'api-key': "03a9d32e584648b69559d0694d8a9a1c",
+ q: searchTerm,
+ begin_date: startYear,
+ end_date: endYear,
+ page: records
+
+});
+$.ajax({
+ url: url,
+ method: 'GET',
+}).done(function(result) {
+ console.log(result);
+}).fail(function(err) {
+ throw err;
+});
+
 });
 
 
